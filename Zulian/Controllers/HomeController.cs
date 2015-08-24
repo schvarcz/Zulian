cf8e4f0284f2f067cee66e8eb1c6f5268ea01b51
Guilherme Schvacz Franco <guilhermefrancosi@gmail.com>
2015-08-24 00:40:16 -0300

    Versão salvando dados em um arquivo texto no servidor.

--- Zulian/Controllers/HomeController.cs
+++ Zulian/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,33 +10,51 @@ namespace Zulian.Controllers
 {
     public class HomeController : Controller
     {
+        static String fileName = "steinmetz.csv";
+        static String path = "~/" + fileName;
         public ActionResult Index()
         {
-            String str = Request.Form.ToString();
-            if (Request.Form.Count > 0)
-                return Redirect("Home/Done");
             return View();
         }
-
-        public ActionResult Done()
+        
+        public ContentResult SeeData()
         {
-            ViewBag.Message = "Your application description page.";
+            TextReader file = new StreamReader(Server.MapPath(path), System.Text.Encoding.UTF8);
+            String str = "<pre>"+file.ReadToEnd()+"</pre>";
+            file.Close();
 
-            return View();
+            return new ContentResult() { Content = str };
         }
 
-        public ActionResult Token()
+        public JsonResult Save()
         {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
+            String str = "";
+            foreach (String key in Request.Form)
+            {
+                if (str != "")
+                    str += ";";
+                str += Request.Form.Get(key);
+            }
+            TextWriter file = new StreamWriter(Server.MapPath(path), true, System.Text.Encoding.UTF8);
+            file.WriteLine(str);
+            file.Close();
+            ViewBag.Message = str;
+
+            return Json(new { Data = str }, JsonRequestBehavior.AllowGet);
         }
 
-        public ActionResult Contact()
+        public ContentResult HardReset()
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            try
+            {
+                TextWriter file = new StreamWriter(Server.MapPath(path), false, System.Text.Encoding.UTF8);
+                file.Write("");
+                file.Close();
+            }
+            catch (Exception e)
+            { }
+            return new ContentResult() { Content = "Arquivo resetado" };
         }
     }
 }
